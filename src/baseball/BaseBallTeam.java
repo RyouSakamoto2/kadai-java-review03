603d5e8359056b38b0894537a1fcf976a7810ff1
smoto <smoto@Ryo-Sakamoto-PC>
2023-02-26 01:03:58 +0900

    オブジェクト指向プログラミング1の課題提出用

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,58 @@
+package baseball;
+
+public class BaseBallTeam {
+ // フィールド
+    private String name;   // 球団名
+    private int win;       // 勝ち数
+    private int lose;      // 負け数
+    private int draw;      // 引き分け数
+
+    // 引数ありコンストラクタ
+    public BaseBallTeam (String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+    
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+    
+    public double getRate() {
+    // 勝率を定義する
+        return (double) this.win / ( this.win + this.lose );
+    }
+    
+    public void report() {
+        System.out.println(this.name + " の2022年の成績は " + this.win + "勝 " + this.lose + "敗 " + this.draw + "分、勝率は " + getRate() + "です。");
+    }
+}
